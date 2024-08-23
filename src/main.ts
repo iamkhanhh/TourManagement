86dbf027a7bed147575951c20f8cbf45cfd45217
Nguyen Khanh <khanh9102004@gmail.com>
2024-08-23 15:12:17 +0700

    init implemention of handlebar view

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,47 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+import * as methodOverride from 'method-override';
+import * as hbs from 'express-handlebars';
+import { ValidationPipe } from '@nestjs/common';
+import * as cors from 'cors';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+  );
+  
+  app.setViewEngine('hbs');
+  app.useStaticAssets(join(__dirname, '..', 'public'), {
+    index: false,
+    prefix: '/public',
+  });
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+
+  app.engine(
+    'hbs',
+    hbs.engine({
+      extname: 'hbs',
+      defaultLayout: 'main',
+      layoutsDir: join(__dirname, '..', 'views', 'layouts'),
+      partialsDir: join(__dirname, '..', 'views', 'partials'),
+      helpers: {}
+    }),
+  );
+ 
+  app.use(cookieParser());
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.use(methodOverride('_method'));
+
+  app.use(cors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
+    credentials: true, 
+  }));
+
   await app.listen(3000);
 }
 bootstrap();
